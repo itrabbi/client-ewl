5650b429867861664d7108caec75f78d13e5579a
itrabbi <hassan.rabbi6321@gmail.com>
2024-02-16 01:41:39 +0600

    404 Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import  Error  from  "../images/404.svg"
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <section className='error-page'>
+        <div className="center">
+          <img className='page-not-found' src={Error} alt="404,Page Not Found" />
+          <Link to="/" className='btn primary'>Go Back Home</Link>
+        </div>
+    </section>
   )
 }
 
