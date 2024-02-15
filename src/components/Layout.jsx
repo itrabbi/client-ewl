3bdbf63b3514226791e3601ee5a90005a40f61f0
itrabbi <hassan.rabbi6321@gmail.com>
2024-02-15 23:14:43 +0600

    Router Setup

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div>
-      Layout
-    </div>
-  )
+    <>
+      <Header />
+        <Outlet />
+      <Footer />
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
