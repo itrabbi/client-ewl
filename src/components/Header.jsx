3813f020c90bac44ee7f07cd739d6492b97924d4
itrabbi <hassan.rabbi6321@gmail.com>
2024-02-17 19:39:45 +0600

    login & logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState ,useContext} from "react";
 import { Link } from "react-router-dom";
 import Logo from "../images/logo.png"
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
+import { UserContext } from '../context/userContext';
 
 function Header() {
   const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false);
+  const {currentUser} = useContext(UserContext);
 
   const  closwNavHandler = () => {
     if(window.innerWidth < 800){
@@ -22,12 +24,18 @@ function Header() {
         <Link to="/" className="nav__logo"  onClick={closwNavHandler}>
           <img src={Logo} alt="EWL" />
         </Link>
-       { isNavShowing && <ul className="nav__menu">
+       { currentUser?.id && isNavShowing && <ul className="nav__menu">
           <li><Link to="/profile/easyweblearner" onClick={closwNavHandler}>Easy Web Learner</Link></li>
           <li><Link to="/create" onClick={closwNavHandler}>Create Posts </Link></li>
           <li><Link to="/authors" onClick={closwNavHandler}>Authors</Link></li>
           <li><Link to="/logout" onClick={closwNavHandler}>Logout</Link></li>
         </ul>}
+
+       { !currentUser?.id && isNavShowing && <ul className="nav__menu">
+          <li><Link to="/authors" onClick={closwNavHandler}>Authors</Link></li>
+          <li><Link to="/login" onClick={closwNavHandler}>login</Link></li>
+        </ul>}
+
         <button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
           {isNavShowing ? <AiOutlineClose /> : <FaBars />}
         </button>
